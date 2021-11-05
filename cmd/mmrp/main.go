3723fa4ac5ce50ac0c044978165a42c3b4dc67b6
kirill <kirill98s@bk.ru>
2021-11-05 23:11:52 +0300

    refactoring

--- cmd/mmrp/main.go
+++ cmd/mmrp/main.go
@@ -1,31 +1,33 @@
 package main
 
 import (
+	"github.com/go-co-op/gocron"
+	"time"
+
 	//"github.com/go-co-op/gocron"
 	//"log"
+	"log"
 	"mmrp-scraper/internal/scrapers"
 	//"time"
 )
 
 func main() {
-	s := scrapers.MAPMScraper{}
-	//s := scrapers.MAPMScraper{}
-	s.Scrape()
-	//if err := run(); err != nil {
-	//	log.Fatal(err)
-	//}
+	if err := run(); err != nil {
+		log.Fatal(err)
+	}
 }
 
-//
-//func run() error {
-//	//s := scrapers.MMRPScraper{}
-//	//scheduler := gocron.NewScheduler(time.UTC)
-//	//_, err := scheduler.Every(15).Minutes().Do(func() {
-//	//	s.Scrape()
-//	//})
-//	//if err != nil {
-//	//	log.Fatal(err)
-//	//}
-//	//scheduler.StartBlocking()
-//	//return nil
-//}
+func run() error {
+	s1 := scrapers.MMRPScraper{}
+	s2 := scrapers.MAPMScraper{}
+	scheduler := gocron.NewScheduler(time.UTC)
+	_, err := scheduler.Every(15).Minutes().Do(func() {
+		s1.Scrape()
+		s2.Scrape()
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	scheduler.StartBlocking()
+	return nil
+}
